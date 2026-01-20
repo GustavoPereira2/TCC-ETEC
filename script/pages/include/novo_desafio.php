0d3a3d3adb3490f5b8432210410580c13135ccea
Gustavo_Pereira2 <psantosgustavo24@gmail.com>
2026-01-20 20:24:41 -0300

    TCC

---
+++ script/pages/include/novo_desafio.php
@@ -0,0 +1,31 @@
+<?php 
+    session_start();
+    require_once "../../dbsolvet.php";
+    if (!isset($_SESSION['user_id'])) {
+        header("Location: ../../../login.html");
+        exit;
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $titulo = trim($_POST['titulo']);
+        $categoria = trim($_POST['categoria']);
+        $conteudo = trim($_POST['conteudo']); 
+
+        if (empty($conteudo)) { 
+        // Conteúdo vazio não deve ser inserido 
+        header("Location: ../home.php"); 
+        exit;
+        }
+
+        $usuario_id = $_SESSION['user_id']; 
+        $stmt = $conexao->prepare("INSERT INTO desafios (usuario_id, titulo, descricao, categoria, criado_em) VALUES (?, ?, ?, ?, DEFAULT)"); 
+        $stmt->bind_param("isss", $usuario_id, $titulo, $conteudo, $categoria); 
+        if ($stmt->execute()) { 
+            header("Location: ../home.php"); 
+            exit; 
+        } else { 
+            echo "Erro ao salvar postagem: " . $stmt->error; 
+        } 
+        $stmt->close();
+    }
+?>
---
+++ script/pages/include/salvar_edicao_solucao.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+require_once "../../dbsolvet.php";
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $comentario_id = $_POST['id'];
+    $comentario = trim($_POST['comentario']);
+
+    $stmt = $conexao->prepare("UPDATE solucoes SET descricao = ? WHERE id = ? AND usuario_id = ?");
+    $stmt->bind_param("sii", $comentario, $comentario_id, $_SESSION['user_id']);
+    $stmt->execute();
+    $stmt->close();
+}
+
+echo "<script>alert('Solução editada com sucesso!');</script>";
+header("Location: ../home.php");
+exit;
